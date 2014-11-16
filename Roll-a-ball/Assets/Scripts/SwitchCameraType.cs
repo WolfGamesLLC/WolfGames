1fec8f76073f1fccf76ff8250f3f57f1b5995cf5
dreinhar <dain@wolfgameslc.com>
2014-11-16 08:59:24 -0500

    Initial load of Unity & MonoDevelop test Roll-a-ball project
    The Roll-a-ball project is a test of the Unity game engine,  the
    MonoDevelop editor and a sandbox for testing of Unity concepts

---
+++ Roll-a-ball/Assets/Scripts/SwitchCameraType.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SwitchCameraType : MonoBehaviour {
+
+	public Camera camera;
+	public Text text;
+
+	public void SetCameraState()
+	{
+		camera.orthographic = !camera.orthographic;
+		text.text = camera.orthographic.ToString();
+	}
+}
