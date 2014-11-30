115da9944ff96b547136f430f9ac8696ffa6e279
dreinhar <dain@wolfgameslc.com>
2014-11-30 09:47:27 -0500

    added new models
    Added Fesant pack
    Added Peasant pack
    Added Medieval tavern pack

---
+++ Aseura/Assets/LoadScene.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+
+	#region Members
+	
+	#endregion
+	
+	#region Properties
+	
+	#endregion
+	
+	/// <summary>
+	/// The main camera.
+	/// </summary>
+	public Fading fadingObject; 
+	
+	#region Methods
+	
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	void Start()
+	{
+		fadingObject = GameObject.Find("Main Camera").GetComponent<Fading>();
+		StartCoroutine(ChangeLevel());
+	}
+	
+	/// <summary>
+	/// Changes the level.
+	/// </summary>
+	/// <returns>The level.</returns>
+	IEnumerator ChangeLevel()
+	{
+		float fadeTime = fadingObject.BeginFade(Fading.FadeDirection.IN);
+		yield return new WaitForSeconds(3);
+	}
+
+	#endregion
+}
--- Aseura/Assets/Scripts/LogoAnimation.cs
+++ Aseura/Assets/Scripts/LogoAnimation.cs
@@ -3,12 +3,56 @@ using System.Collections;
 
 public class LogoAnimation : MonoBehaviour 
 {
-	// Update is called once per frame
+	#region Members
+
+	#endregion
+	
+	#region Properties
+	
+	#endregion
+	
+	/// <summary>
+	/// The main camera.
+	/// </summary>
+	public Fading fadingObject; 
+	
+	#region Methods
+
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	void Start()
+	{
+		fadingObject = GameObject.Find("Main Camera").GetComponent<Fading>();
+	}
+
+	/// <summary>
+	/// Update this instance.
+	/// </summary>
 	void Update () 
 	{
-		if(transform.localScale.x > 1)
+		if(transform.localScale.x > 2)
 		{
 			transform.localScale += new Vector3(-1,-1,-1);
 		}
+
+		if(transform.localScale.x <= 2)
+		{
+			StartCoroutine(ChangeLevel());
+		}
 	}
+
+	/// <summary>
+	/// Changes the level.
+	/// </summary>
+	/// <returns>The level.</returns>
+	IEnumerator ChangeLevel()
+	{
+		float fadeTime = fadingObject.BeginFade(Fading.FadeDirection.OUT);
+		yield return new WaitForSeconds(3);
+
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+
+	#endregion
 }
