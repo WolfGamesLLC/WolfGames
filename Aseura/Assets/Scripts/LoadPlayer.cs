0743a7e5a3e39eff4e07688ff953d69398b6f100
dreinhar <dain@wolfgameslc.com>
2015-01-18 09:14:43 -0500

    Added world selection screen
    Added connection text
    Reworked launch of world selection screen
    Removed WorldListInputField
    Added Remote World List label
    Renamed Local World List label

--- Aseura/Assets/Scripts/LoadPlayer.cs
+++ Aseura/Assets/Scripts/LoadPlayer.cs
@@ -4,62 +4,104 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-public class LoadPlayer : MonoBehaviour 
+public class LoadPlayer : MonoBehaviour
 {
+    #region Members
     
-	public InputField PlayerNameInputField;
-	public Text LoadButtonText;
-	public GameObject PlayerViewPanel;
-    public GameObject LaunchScreenPanel;
     private List<LevelSerializer.SaveEntry> sg;
+    
+    #endregion
 
-	public void Start()
-	{
-		SetLoadGameButtonText ();
-	}
+    #region Editor Properties
 
-	void SetLoadGameButtonText ()
-	{
-		sg = LevelSerializer.SavedGames [LevelSerializer.PlayerName];
-		if (sg.Count >= 1) 
-		{
-			LoadButtonText.text = sg [0].Caption;
-		}
-		else
-			LoadButtonText.text = "Create New Player";
-	}
+    [SerializeField] 
+    private InputField PlayerNameInputField;
 
-	public void LoadPlayerData()
-	{
+    [SerializeField] 
+    private Text LoadButtonText;
+
+    [SerializeField] 
+    private GameObject PlayerViewPanel;
+
+    [SerializeField] 
+    private GameObject LaunchScreenPanel;
+
+    [SerializeField]
+    private GameObject WorldSelectionPanel;
+    
+    #endregion
+
+    #region Properties
+    #endregion
+
+    #region Initialization
+
+    /// <summary>
+    /// Initialize the LoadPlayer componenet
+    /// </summary>
+    public void Start()
+    {
+        SetLoadGameButtonText();
+    }
+    
+    #endregion
+
+    #region Methods
+
+    void SetLoadGameButtonText()
+    {
+        sg = LevelSerializer.SavedGames[LevelSerializer.PlayerName];
+        if (sg.Count >= 1)
+        {
+            LoadButtonText.text = sg[0].Caption;
+        }
+        else
+            LoadButtonText.text = "Create New Player";
+    }
+
+    /// <summary>
+    /// Load player data and activate the next screen (I should not be doing this here)
+    /// </summary>
+    public void LoadPlayerData()
+    {
         LaunchScreenPanel.SetActive(false);
 
         if (sg.Count >= 1)
-		{
-			LevelSerializer.LoadNow(sg[0].Data);
+        {
+            LevelSerializer.LoadNow(sg[0].Data);
             PlayerViewPanel.SetActive(false);
-		}
-		else
-		{
+            WorldSelectionPanel.SetActive(true);
+        }
+        else
+        {
             PlayerViewPanel.SetActive(true);
-		}
+        }
 
-		Time.timeScale = 1;
-	}
+        Time.timeScale = 1;
+    }
 
-	public void SavePlayerData()
-	{
-		if(PlayerNameInputField.text != "")
-			LevelSerializer.SaveGame(PlayerNameInputField.text);
-		else
-			LevelSerializer.SaveGame("Player_1");
-	}
+    /// <summary>
+    /// Save the player's data
+    /// </summary>
+    public void SavePlayerData()
+    {
+        if (PlayerNameInputField.text != "")
+            LevelSerializer.SaveGame(PlayerNameInputField.text);
+        else
+            LevelSerializer.SaveGame("Player_1");
+    }
 
-	public void DeletePlayerData()
-	{
-		if(sg.Count >= 1)
-		{
-			sg[0].Delete();
-			SetLoadGameButtonText();
-		}
-	}
+    /// <summary>
+    /// Delete a player's saved data
+    /// </summary>
+    public void DeletePlayerData()
+    {
+        if (sg.Count >= 1)
+        {
+            sg[0].Delete();
+            SetLoadGameButtonText();
+        }
+    }
+    
+    #endregion
 }
