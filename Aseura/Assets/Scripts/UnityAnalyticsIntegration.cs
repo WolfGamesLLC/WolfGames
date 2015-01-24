c31807cb5722e8373d1176f379a2078def00fa46
dreinhar <dain@wolfgameslc.com>
2015-01-24 10:56:41 -0500

    Added saving of world data

---
+++ Aseura/Assets/Scripts/UnityAnalyticsIntegration.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Cloud.Analytics;
+
+public class UnityAnalyticsIntegration : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+        const string projectId = "PROJECTID-GOES-HERE-66fb5cf028d728bb";
+        UnityAnalytics.StartSDK(projectId);
+
+    }
+
+}
