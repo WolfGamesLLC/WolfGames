0cbdae679522f70b6f0f460b5a5f6a0359a0e5f0
Dain Reinhart <dain@wolfgamesllc.com>
2016-09-21 08:01:22 -0400

    Now add to the score when the ball is moving although it is not dependent on the speed.

--- Marble Motion/Assets/Scripts/PlayerController.cs
+++ Marble Motion/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float scoreModifier;
+    public Text scoreText;
+
     private Rigidbody rB;
+    private float score;
 
     // Initialize the object
     public void Start()
     {
         rB = GetComponent<Rigidbody>();
+        score = 0;
+        scoreText.text = score.ToString();
     }
 
     // Update is called once per frame
@@ -19,6 +26,11 @@ public class PlayerController : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
 
         rB.AddForce(new Vector3(moveHorizontal, 0.0f, moveVertical) * speed);
+        if (rB.IsSleeping() == false)
+        {
+            score += scoreModifier * speed;
+            scoreText.text = score.ToString();
+        }
     }
 
     // OnTriggerEnter is called when the Collider other enters the trigger
