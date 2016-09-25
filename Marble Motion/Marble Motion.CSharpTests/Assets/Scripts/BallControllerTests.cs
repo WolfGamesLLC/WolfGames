ae4b50bec854f85134f8ac5a635fade5f24d04bf
Dain Reinhart <dain@wolfgamesllc.com>
2016-09-25 08:54:16 -0400

    Add unit testing to BallController

---
+++ Marble Motion/Marble Motion.CSharpTests/Assets/Scripts/BallControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class BallControllerTests
+    {
+        [TestMethod()]
+        public void MoveTest()
+        {
+            Assert.Fail();
+        }
+        
+        [TestMethod()]
+        public void SetScoreTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetMovementControllerTest()
+        {
+            MockPlayerController testPlayer = new MockPlayerController();
+            BallController testBallController = new BallController();
+
+            testBallController.SetMovementController(testPlayer);
+
+            PrivateObject pObject = new PrivateObject(testBallController);
+            Assert.AreSame(testPlayer, pObject.GetFieldOrProperty("movementController"));
+        }
+
+        [TestMethod()]
+        public void SetScoreControllerTest()
+        {
+            MockPlayerController testPlayer = new MockPlayerController();
+            BallController testBallController = new BallController();
+
+            testBallController.SetScoreController(testPlayer);
+
+            PrivateObject pObject = new PrivateObject(testBallController);
+            Assert.AreSame(testPlayer, pObject.GetFieldOrProperty("scoreController"));
+        }
+
+        private IMovementController GetMovementMock()
+        {
+            return new MockPlayerController();
+        }
+    }
+}
---
+++ Marble Motion/Marble Motion.CSharpTests/Assets/Scripts/MockPlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Tests
+{
+    class MockPlayerController : IMovementController, IScoreController
+    {
+        public Vector3 MovedForce;
+
+        public void Move(Vector3 force)
+        {
+            MovedForce = force;
+        }
+
+        public void Set(float score)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
