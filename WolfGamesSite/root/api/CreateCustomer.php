adf26448d61415375f13d2220d3222a3911a123c
Dain Reinhart <dain@wolfgamesllc.com>
2016-10-29 09:09:20 -0400

    Added api/CreateCustomer.php Replaced bad web.config with a new working one

---
+++ WolfGamesSite/root/api/CreateCustomer.php
@@ -0,0 +1,20 @@
+﻿<?php
+        $db = mysql_connect('68.178.217.40', 'wolfmaindb', 'Acc3ss@M41n') or die('Could not connect: ' . mysql_error());
+        mysql_select_db('wolfmaindb') or die('Could not select database');
+
+        // Strings must be escaped to prevent SQL injection attack.
+        $lastName = mysql_real_escape_string($_GET['lastName'], $db);
+        $score = mysql_real_escape_string($_GET['score'], $db);
+        $hash = $_GET['hash'];
+
+        $secretKey="WolfGamesSecretKey"; # Change this value to match the value stored in the client javascript below
+
+        $real_hash = md5($position_x . $position_y . $score . $secretKey);
+//        if($real_hash == $hash) {
+            // Send variables for the MySQL database class.
+            $query = "insert into Customer values (NULL, NULL, NULL, NULL, '$lastName', NULL, NULL, NULL, '$score');";
+            $result = mysql_query($query) or die('Query failed: ' . mysql_error());
+//        }
+//        echo "pos_x = {" . $position_x . "}\tpos_y = {" . $position_y . "}\tscore = {" . $score . "}\n";
+//        echo "real_hash = {" . $real_hash . "}\thash = {" . $hash . "}\n"; 
+?>
