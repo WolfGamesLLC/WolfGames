a5ed9c911893eb04e4bb3dbe834100b11ad79417
Dain Reinhart <dain@wolfgamesllc.com>
2016-11-12 15:00:55 -0500

    Added WGDbContext

---
+++ WolfGamesSite/WolfGamesSite/Models/WGDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WolfGamesSite.Models
+{
+    public class WGDbContext : DbContext
+    {
+        public const string DefaultConnection = "DefaultConnection";
+        public WGDbContext() : base(DefaultConnection)
+        { }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
