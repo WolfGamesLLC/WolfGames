4c5eb72b3c98704c99113cd5e8b15595c48ae9d1
Dain Reinhart <dain@wolfgamesllc.com>
2016-11-12 14:28:13 -0500

    Site runs on www.wolfgamesllc.com and can debug

---
+++ WolfGamesProjectSite/WolfGamesProjectSite/Models/WGDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WolfGamesProjectSite.Models
+{
+    public class WGDbContext : DbContext
+    {
+        public WGDbContext() : base("DefaultConnection")
+        { }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
