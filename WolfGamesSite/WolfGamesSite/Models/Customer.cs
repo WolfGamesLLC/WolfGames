12fc5a938c5c90de2a2a98c325e00fdba0ee1888
Dain Reinhart <dain@wolfgamesllc.com>
2016-11-12 14:55:31 -0500

    Added Customer model to site

---
+++ WolfGamesSite/WolfGamesSite/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WolfGamesSite.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public DateTime DttmCreated { get; set; }
+        public DateTime DttmUpdated { get; set; }
+        public DateTime BirthDay { get; set; }
+        public long Score { get; set; }
+    }
+}
