e17222f56b5651d92d0d0577c67ab6e7249cfff4
Dain Reinhart <dain@wolfgamesllc.com>
2017-04-24 16:59:43 -0400

    Added about text to home controller

--- WolfGamesSite/WolfGamesSite.Tests/Controllers/HomeControllerTest.cs
+++ WolfGamesSite/WolfGamesSite.Tests/Controllers/HomeControllerTest.cs
@@ -35,7 +35,7 @@ namespace WolfGamesSite.Tests.Controllers
             ViewResult result = controller.About() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+            Assert.AreEqual("About Wolf Games.", result.ViewBag.Message);
         }
 
         [TestMethod]
