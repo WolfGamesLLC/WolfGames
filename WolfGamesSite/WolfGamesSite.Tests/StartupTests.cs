d477087649e0316017046bc20e1439f526df3d77
Dain Reinhart <dain@wolfgamesllc.com>
2017-05-15 06:27:34 -0400

    Added StartupTests to test Startup class

---
+++ WolfGamesSite/WolfGamesSite.Tests/StartupTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WolfGamesSite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WolfGamesSite.Tests
+{
+    [TestClass()]
+    public class StartupTests
+    {
+        [TestMethod()]
+        public void StartupTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ConfigurationTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
