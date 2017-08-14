e62eeb1331936ad2dd84f3d060db628bc8605610
Dain Reinhart <dain@wolfgamesllc.com>
2017-08-14 16:36:20 -0400

    Added initilal DB load migrations

--- WolfGamesCoreSite/WolfGamesCoreSite/Data/Migrations/20170812175110_NewDB.Designer.cs
+++ WolfGamesCoreSite/WolfGamesCoreSite/Data/Migrations/20170812175110_NewDB.Designer.cs
@@ -8,9 +8,10 @@ using WolfGamesCoreSite.Data;
 namespace WolfGamesCoreSite.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170812175110_NewDB")]
+    partial class NewDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
