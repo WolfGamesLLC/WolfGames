47bc6bf8d25908555c4b11b0dca9c8ec6009b1c9
Dain Reinhart <dain@wolfgamesllc.com>
2017-08-27 06:58:10 -0400

    Renamed WolfGamesSite.XUnitTest.UnitTest1 to HomeControllerTestSuite

---
+++ WolfGamesSite/WolfGamesSite.XUnitTest/HomeControllerTestSuite.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace WolfGamesSite.XUnitTest
+{
+    public class HomeControllerTestSuite
+    {
+        [Fact]
+        public void ShouldCreateHomeController()
+        {
+            Assert.False(true);
+        }
+    }
+}
