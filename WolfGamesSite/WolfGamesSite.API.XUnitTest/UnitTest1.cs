7fe4895e786b685a3741321894ee53ec21641a28
Dain Reinhart <dain@wolfgamesllc.com>
2017-08-27 08:35:53 -0400

    Added WolfGamesSite.API.XUnitTest

---
+++ WolfGamesSite/WolfGamesSite.API.XUnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace WolfGamesSite.API.XUnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
