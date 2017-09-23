9cdae8e21982e8002874e1dbcd29e6ebc6b40f91
Dain Reinhart <dain@wolfgamesllc.com>
2017-09-23 11:58:59 -0400

    Updated HomeController.About action Added HomeControllerMessages class

--- WolfGamesSite/WolfGamesSite.XUnitTest/HomeControllerTestSuite.cs
+++ WolfGamesSite/WolfGamesSite.XUnitTest/HomeControllerTestSuite.cs
@@ -7,20 +7,113 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WolfGamesSite.XUnitTest
 {
+    /// <summary>
+    /// Test suite for the HomeControllerMessage object
+    /// </summary>
+    public class HomeControllerMessageShould
+    {
+        /// <summary>
+        /// The default constructor should set message to text
+        /// </summary>
+        [Fact]
+        public void HomeControllerMessagesTest()
+        {
+            string text = "test text";
+            Assert.Equal(text, new HomeControllerMessages(text).Message);
+        }
+
+        /// <summary>
+        /// The About method should return about text
+        /// </summary>
+        [Fact]
+        public void AboutTest()
+        {
+            Assert.Equal("About Wolf Games.", HomeControllerMessages.About());
+        }
+
+        /// <summary>
+        /// The Contact method should return contact text
+        /// </summary>
+        [Fact]
+        public void ContactTest()
+        {
+            Assert.Equal("We love to hear from you.", HomeControllerMessages.Contact());
+        }
+
+        /// <summary>
+        /// The Error method should return error text
+        /// </summary>
+        [Fact]
+        public void ErrorTest()
+        {
+            Assert.Equal("~/Views/Shared/Error.cshtml", HomeControllerMessages.Error());
+        }
+
+        /// <summary>
+        /// The ThankYou method should return thank you text
+        /// </summary>
+        [Fact]
+        public void ThankYouTest()
+        {
+            Assert.Equal("Thank you from all of us at Wolf Games.", HomeControllerMessages.ThankYou());
+        }
+
+        /// <summary>
+        /// The DevCorner method should return dev corner intro text
+        /// </summary>
+        [Fact]
+        public void DevCornerTest()
+        {
+            Assert.Equal("Welcome to Wolf Games' dev corner.", HomeControllerMessages.DevCorner());
+        }
+    }
+
+    /// <summary>
+    /// Test suite for the home controller
+    /// </summary>
     public class HomeControllerShould
     {
+        HomeController Controller;
+        ViewResult Result;
+
+        /// <summary>
+        /// The test initializer for the home controller test suite
+        /// </summary>
+        public HomeControllerShould()
+        {
+            Controller = new HomeController();
+            ViewResult Result;
+        }
+
+        /// <summary>
+        /// The index action should return a ViewResult
+        /// </summary>
         [Fact]
         public void IndexReturnsViewResult()
         {
-            var result = new HomeController().Index();
-            Assert.IsType<ViewResult>(result);
+            Result = Controller.Index() as ViewResult;
+            Assert.IsType<ViewResult>(Result);
         }
 
+        /// <summary>
+        /// The DevCorner action should return a ViewResult
+        /// </summary>
         [Fact]
         public void DevCornerReturnsViewResult()
         {
-            var result = new HomeController().DevCorner();
-            Assert.IsType<ViewResult>(result);
+            Result = Controller.DevCorner() as ViewResult;
+            Assert.IsType<ViewResult>(Result);
+        }
+
+        /// <summary>
+        /// The about action returns a ViewResult
+        /// </summary>
+        [Fact]
+        public void AboutReturnsViewResultWithAboutMessage()
+        {
+            Result = Controller.About() as ViewResult;
+            Assert.IsType<ViewResult>(Result);
+            Assert.Equal(HomeControllerMessages.About(), Result.ViewData["Message"]);
         }
     }
 }
